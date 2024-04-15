24d24547b4dcdd4f452f0dcb354356295d6cd489
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-04-15 16:46:29 +0530

    removed php

--- submit_form.php
+++ submit_form.php
@@ -1,11 +1,11 @@
-<?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $Phone = $_POST['Phone'];
-    $message = $_POST['message'];
+//<?php
+//if($_SERVER["REQUEST_METHOD"] == "POST") {
+//    $name = $_POST['name'];
+//    $Phone = $_POST['Phone'];
+//    $message = $_POST['message'];
 
 
-    header("Location: thank-you.html");
-    exit();
-}
-?>
+//    header("Location: thank-you.html");
+//    exit();
+//}
+//?>
