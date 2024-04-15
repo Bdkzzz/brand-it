b64e70cf4f10b11c9ab7d327cdefabe10e419fe2
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-04-15 14:55:44 +0530

    php and form additions

---
+++ submit_form.php
@@ -0,0 +1,11 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $Phone = $_POST['Phone'];
+    $message = $_POST['message'];
+
+
+    header("Location: thank-you.html");
+    exit();
+}
+?>
